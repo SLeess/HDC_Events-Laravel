0e1cdf8267f9b969df4baa15631f9525f0a59ad5
Le <duraesleandro21@gmail.com>
2023-12-27 01:51:36 -0300

    Em andamento - Autenticação, Perfil, Relações de banco

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Event;
+use App\Models\User;
 
 class EventController extends Controller
 {
@@ -46,12 +47,17 @@ class EventController extends Controller
             $event->image = $imageName;
         }
 
+        $event->user_id = auth()->user()->id;
+
         $event->save();
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
     }
 
     public function show($id){
         $event = Event::findOrFail($id);
-        return view('events.show', ['event' => $event]);
+
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
+
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
     }
 }
--- hdcevents/resources/views/layouts/main.blade.php
+++ hdcevents/resources/views/layouts/main.blade.php
@@ -31,8 +31,22 @@
                     <ul class="navbar-nav">
                         <li class="nav-item"><a href="/" class="nav-link">Eventos</a></li>
                         <li class="nav-item"><a href="/events/create" class="nav-link">Criar Eventos</a></li>
-                        <li class="nav-item"><a href="/" class="nav-link">Entrar</a></li>
-                        <li class="nav-item"><a href="/" class="nav-link">Cadastrar</a></li>
+                        @auth
+                        <li class="nav-item"><a href="/dashboard" class="nav-link">Meus Eventos</a></li>
+                        <li class="nav-item">
+                            <form action="/logout" method="post">
+                                @csrf
+                                <a href="/logout" class="nav-link" onclick="
+                                    event.preventDefault();
+                                    this.closest('form').submit();
+                                    ">Sair</a>
+                            </form>
+                        </li>
+                        @endauth
+                        @guest
+                        <li class="nav-item"><a href="/login" class="nav-link">Entrar</a></li>
+                        <li class="nav-item"><a href="/register" class="nav-link">Cadastrar</a></li>
+                        @endguest
                         <li class="nav-item"><a href="/contato" class="nav-link">Contato</a></li>
                     </ul>
                 </div>
